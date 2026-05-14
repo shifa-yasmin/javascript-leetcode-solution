6d95da092bc694b43813f939883bc06235657075
shifa-yasmin <shifayasmin23@gmail.com>
2026-05-14 10:07:44 +0530

    Time: 1 ms (76.58%) | Memory: 53.3 MB (91.44%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} title
+ * @return {string}
+ */
+var capitalizeTitle = function(title) {
+    return title
+        .split(" ")
+        .map(word => {
+            if (word.length <= 2) {
+                return word.toLowerCase();
+            }
+            return word.charAt(0).toUpperCase() +
+                   word.slice(1).toLowerCase();
+        })
+        .join(" ");
+};
