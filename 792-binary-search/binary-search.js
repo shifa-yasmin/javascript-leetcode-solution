07f9a580159103a5bade88cba5fa31ead19a10e1
shifa-yasmin <shifayasmin23@gmail.com>
2026-05-27 23:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 58.5 MB (9.98%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function(nums, target) {
+   return nums.indexOf(target)
+};
+console.log(search([-1,0,3,5,9,12],9))
+console.log(search([-1,0,3,5,9,12],2))
