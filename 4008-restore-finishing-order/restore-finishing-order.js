5c56ae4a1a7c403818d0f7adf802503678569a29
shifa-yasmin <shifayasmin23@gmail.com>
2026-06-02 09:32:39 +0530

    Time: 2 ms (40.34%) | Memory: 57.4 MB (43.79%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+var recoverOrder = function(order, friends) {
+        let res=[]
+     for(let i=0;i<order.length;i++)  {
+        if(friends.includes(order[i])){
+         res.push(order[i])
+        }
+     } 
+     return res;
+    
+};
