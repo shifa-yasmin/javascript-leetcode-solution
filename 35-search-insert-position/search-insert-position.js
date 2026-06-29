52ef2b2ed872fa0ba104cff8859c2323fa1ebac0
shifa-yasmin <shifayasmin23@gmail.com>
2026-06-29 10:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (95.16%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target)
+    }else{
+        for(i=0;i<nums.length;i++){
+            if(nums[i]>target){
+                return i;
+            }
+        }
+        return nums.length
+    }
+};
+console.log(searchInsert([1,3,5,6],5))
+console.log(searchInsert([1,3,5,6],2))
+console.log(searchInsert([1,3,5,6],7))
