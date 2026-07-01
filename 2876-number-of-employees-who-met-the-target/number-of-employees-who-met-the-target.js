9f1505f30d43d235134bdec91202fd1cb18b4750
shifa-yasmin <shifayasmin23@gmail.com>
2026-07-01 09:37:30 +0530

    Time: 1 ms (27.27%) | Memory: 56.3 MB (32.17%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+let count=0;
+for(let h of hours){
+    if(h>=target){
+        count++;
+    }
+}
+return count;
+};
+console.log(numberOfEmployeesWhoMetTarget([0,1,2,3,4],2))
+console.log(numberOfEmployeesWhoMetTarget([5,1,4,2,2],6))
