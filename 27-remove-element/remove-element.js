2e5e44c9db14a2b79158d0666f877b7aa9e55ed8
shifa-yasmin <shifayasmin23@gmail.com>
2026-07-09 15:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (60.36%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let count=0;
-      for(i=0;i<nums.length;i++)  {
-              if(nums[i]!==val){
-                nums[count]=nums[i]
-                count++
-              }
-      }
-       return count
+let k=0;
+for(i=0;i<nums.length;i++){
+    if(nums[i]!==val){
+        nums[k]=nums[i];
+        k++;
+    }
+}
+return k;
 };
-console.log(removeElement([3,2,2,3], 3))
-console.log(removeElement([0,1,2,2,3,0,4,2], 2))
+console.log(removeElement([3,2,2,3],3))
+console.log(removeElement([0,1,2,2,3,0,4,2],2))
