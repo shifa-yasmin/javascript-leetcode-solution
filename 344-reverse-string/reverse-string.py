af96471c1901f7824664b39d39bd9f915110a975
shifa-yasmin <shifayasmin23@gmail.com>
2026-08-03 19:11:44 +0530

    Time: 167 ms (5.01%) | Memory: 19.7 MB (76.79%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -2,6 +2,7 @@ class Solution(object):
     def reverseString(self, s):
         s.reverse()
         print(s)
+        
 obj = Solution()  
 obj.reverseString(["h","e","l","l","o"])
 obj.reverseString(["H","a","n","n","a","h"])
